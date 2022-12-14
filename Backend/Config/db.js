583c381a00ac728394074feadd0e9decf2686897
Praful-87 <103850217+Praful-87@users.noreply.github.com>
2022-12-14 21:44:18 +0530

    ok

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
 require("dotenv").config();
-const connection = mongoose.connect(
-  "mongodb+srv://praful:praful@cluster0.q1wzc7s.mongodb.net/appleTv?retryWrites=true&w=majority"
-);
+const connection = mongoose.connect();
 module.exports = { connection };
