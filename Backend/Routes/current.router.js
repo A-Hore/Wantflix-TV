8490923d460b9c3409ee37195db96429aefb50a8
Praful-87 <103850217+Praful-87@users.noreply.github.com>
2022-12-14 21:33:17 +0530

    ok

---
+++ Backend/Routes/current.router.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const { CurrentModel } = require("../Models/Current.model");
+const currentRouter = Router();
+currentRouter.get("/", async (req, res) => {
+  try {
+    let currentMovie = await CurrentModel.find();
+    console.log(currentMovie);
+    res.send(currentMovie);
+  } catch (err) {
+    res.send({ msg: "something went wrong" });
+    console.log(err);
+  }
+});
+module.exports = { currentRouter };
