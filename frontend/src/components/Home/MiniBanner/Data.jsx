b55672db19b447e8416425d492d7009da4eb89ce
sdibyanshu <sdibyanshu>
2022-12-16 14:41:19 +0530

    mini slide show updated

---
+++ frontend/src/components/Home/MiniBanner/Data.jsx
@@ -0,0 +1,44 @@
+const jumboSliderData = [
+    {
+      image:
+        "https://c4.wallpaperflare.com/wallpaper/107/848/913/spiderman-ps4-spiderman-games-hd-wallpaper-preview.jpg",
+    },
+    {
+      image:
+        "https://img.freepik.com/free-vector/online-movies-entertainment-banner_1419-2246.jpg?w=1060&t=st=1671181466~exp=1671182066~hmac=27caa9b0434013de9e5e4bcb2bbe18f7409a3e285f8b173ca8d65f10e533164a",
+    },
+    {
+      image:
+        "https://is3-ssl.mzstatic.com/image/thumb/Features126/v4/38/ea/d8/38ead8ac-0cb6-a41f-fcc4-25b0e3e92ff1/RVNXLVRWQS1XVy1Ucm9waHlfQ2FzZV9RdW90ZV9TZXZlcmFuY2UucG5n.png/1320x495sr.webp",
+    },
+    {
+      image:
+        "https://is5-ssl.mzstatic.com/image/thumb/Features116/v4/9d/ba/f8/9dbaf8be-aa79-eece-b465-497c7d9fd1fa/RVNXLVRWQS1XVy1Ucm9waHlfQ2FzZV9RdW90ZS1UaGVfQWZ0ZXJwYXJ0eS5wbmc.png/1320x495sr.webp",
+    },
+    {
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features126/v4/85/3c/c0/853cc067-fb76-5346-2411-705fcc8203de/RVNXLVRWQS1XVy1Td2FuX1NvbmctTkFBQ1BfQXdhcmQucG5n.png/1320x495sr.webp",
+    },
+    {
+      image:
+        "https://is5-ssl.mzstatic.com/image/thumb/Features126/v4/fa/bf/a4/fabfa44e-2077-90ff-dfa2-23fc70e1bfa1/RVNXLVRWQS1XVy1UTVMtU0FHX0F3YXJkLnBuZw.png/1320x495sr.webp",
+    },
+    {
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features125/v4/9b/70/94/9b709448-8024-7e0b-57f8-d551cdbd9c01/RVNXLVRWQS1XVy1Ucm9waHlfQ2FzZS1UZWRfTGFzc29fUzItbGFuZz1lbl91cy5wbmc.png/1320x495sr.webp",
+    },
+    {
+      image:
+        "https://is1-ssl.mzstatic.com/image/thumb/Features116/v4/7f/50/36/7f5036da-fcf2-21b0-5c22-e1a137459a90/RVNXLVRWQS1XVy1Ucm9waHlfQ2FzZV9RdW90ZS1GaW5jaC1sYW5nPWVuX3VzLnBuZw.png/1320x495sr.webp",
+    },
+    {
+      image:
+        "https://is4-ssl.mzstatic.com/image/thumb/Features115/v4/b0/e3/6e/b0e36ee0-6bd7-107d-a106-f06c28c5607e/RVNXLVRWQS1XVy1Ucm9waHlfQ2FzZV9RdW90ZS1Gb3JfQWxsX01hbmtpbmQtbGFuZz1lbl91cy5wbmc.png/1320x495sr.webp",
+    },
+  ];
+
+  export {
+  
+    jumboSliderData,
+    
+  };
