84f46d66fa9f6b05dccfb890fff6de0c71d4211f
sdibyanshu <sdibyanshu>
2022-12-18 15:42:35 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,5 @@
-// import logo from "./logo.svg";
-import "./App.css";
-<<<<<<< HEAD
 
-import Login from "./components/Login/Login";
-=======
->>>>>>> cd23a5015b09b0d46d4746130612968a77af07f1
-// import Home from "./components/Home/Home";
+import "./App.css";
 import Header from "./components/Navbar/Navbar";
 import MainRoute from "./components/Route/MainRoute";
 
@@ -13,7 +7,6 @@ import MainRoute from "./components/Route/MainRoute";
 function App() {
   return (
     <div className="App">
-      {/* <h1>SOHIL</h1> */}
       <Header />
       <MainRoute />
   
